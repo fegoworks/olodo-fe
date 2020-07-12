ea7c5704aef35c61ca2d334ef9b18c097c09ee3d
unknown <fegoworks@hotmail.com>
2020-07-12 15:11:57 +0100

    chore(folder): set up file and folder structure

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ThemeWrapper from '<styles>/ThemeWrapper/ThemeWrapper';
+import GlobalStyle from '<styles>/global';
+import BaseRoutes from '<pages>/index.jsx';
+
+const App = () => (
+  <ThemeWrapper>
+    <Fragment>
+      <GlobalStyle />
+      <Router>
+        <BaseRoutes />
+      </Router>
+    </Fragment>
+  </ThemeWrapper>
+);
+
+export default App;
+
+render(<App />, document.getElementById('app'));
