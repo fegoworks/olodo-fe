5ed6b40e58f31b331216eac2e3dcdb9450a0cd89
unknown <fegoworks@hotmail.com>
2020-07-12 15:10:01 +0100

    chore(styles): set up reusable css style declarations

---
+++ src/styles/variables/mainTheme.js
@@ -0,0 +1,19 @@
+import {
+  fontSize,
+  fontWeight
+} from './fonts'
+import {
+  spacing
+} from './spacing'
+import {
+  textColors,
+  backgroundColors
+} from './colorPalette'
+
+export default {
+  fontSize,
+  fontWeight,
+  spacing,
+  textColors,
+  backgroundColors
+}
