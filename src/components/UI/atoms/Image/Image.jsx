1aac7fd22a1b94707e69dbd8c7da639b6fd2f647
unknown <fegoworks@hotmail.com>
2020-07-15 22:34:42 +0100

    feat(i<div id=book_details class=book_details>): build out component

---
+++ src/components/UI/atoms/Image/Image.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { spacing } from '<styles>/variables/spacing';
+
+const Image = ({ imageUrl, altTitle, width }) => (
+  <Image.Container src={imageUrl} alt={altTitle} width={width} />
+);
+
+Image.Container = styled.img`
+  width: ${(props) => props.theme.spacing[props.width] || '100%'};
+  border-radius: 0.25rem 0.25rem 0 0;
+`;
+
+Image.propTypes = {
+  imageUrl: PropTypes.string,
+  altTitle: PropTypes.string,
+  width: PropTypes.oneOf(Object.keys(spacing)),
+};
+
+export default Image;
