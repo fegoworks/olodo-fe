330201e3253270758e35177dddee60257510deee
unknown <fegoworks@hotmail.com>
2020-07-15 22:37:33 +0100

    feat(image): build out atomic component

--- src/components/UI/atoms/Image/Image.jsx
+++ src/components/UI/atoms/Image/Image.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
 import { spacing } from '<styles>/variables/spacing';
 
 const Image = ({ imageUrl, altTitle, width }) => (
