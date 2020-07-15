818ab08891eac9ce774ea7f7aa4bd6fce9672b87
unknown <fegoworks@hotmail.com>
2020-07-15 22:32:46 +0100

    feat(sidebar): build out component

---
+++ src/components/UI/organisms/Sidebar.jsx
@@ -0,0 +1,36 @@
+/** @format */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import SidebarList from '<molecules>/SidebarList';
+import SidebarItem from '<molecules>/SidebarItem';
+
+const Sidebar = () => (
+  <Sidebar.Container>
+    <SidebarList title="Explore">
+      <SidebarItem item="Books" itemSize="273" />
+      <SidebarItem item="Genres" itemSize="33" />
+      <SidebarItem item="Authors" itemSize="60" />
+    </SidebarList>
+    <SidebarList title="My Books">
+      <SidebarItem item="Books" itemSize="273" />
+      <SidebarItem item="Genres" itemSize="33" />
+    </SidebarList>
+    <SidebarList title="Explore">
+      <SidebarItem item="Books" itemSize="273" />
+      <SidebarItem item="Genres" itemSize="33" />
+    </SidebarList>
+  </Sidebar.Container>
+);
+
+Sidebar.Container = styled.aside`
+  position: fixed;
+  width: 20rem;
+  top: 7rem;
+  -webkit-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+  -moz-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+  box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+`;
+
+export default Sidebar;
