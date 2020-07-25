192e67e00f72c7fb5407fcc0213e4167fe352e81
unknown <fegoworks@hotmail.com>
2020-07-25 15:16:53 +0100

    feat(sidebar): modify sidebar as part of hamburger menu

--- src/components/UI/organisms/Sidebar.jsx
+++ src/components/UI/organisms/Sidebar.jsx
@@ -1,39 +1,48 @@
 /** @format */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import SidebarList from '<molecules>/SidebarList';
 import SidebarItem from '<molecules>/SidebarItem';
+import { HamburgerContext } from '<state>/HamburgerContext';
 
-const Sidebar = () => (
-  <Sidebar.Container>
-    <SidebarList title="Explore">
-      <SidebarItem item="Books" itemSize="273" />
-      <SidebarItem item="Genres" itemSize="33" />
-      <SidebarItem item="Authors" itemSize="60" />
-    </SidebarList>
-    <SidebarList title="My Books">
-      <SidebarItem item="Books" itemSize="273" />
-      <SidebarItem item="Genres" itemSize="33" />
-    </SidebarList>
-    <SidebarList title="Explore">
-      <SidebarItem item="Books" itemSize="273" />
-      <SidebarItem item="Genres" itemSize="33" />
-    </SidebarList>
-  </Sidebar.Container>
-);
+const Sidebar = () => {
+  const { open } = useContext(HamburgerContext);
+
+  return (
+    <Sidebar.Container open={open}>
+      <SidebarList title="Explore">
+        <SidebarItem item="Books" itemSize="273" />
+        <SidebarItem item="Genres" itemSize="33" />
+        <SidebarItem item="Authors" itemSize="60" />
+      </SidebarList>
+      <SidebarList title="My Books">
+        <SidebarItem item="Books" itemSize="273" />
+        <SidebarItem item="Genres" itemSize="33" />
+      </SidebarList>
+      <SidebarList title="Explore">
+        <SidebarItem item="Books" itemSize="273" />
+        <SidebarItem item="Genres" itemSize="33" />
+      </SidebarList>
+    </Sidebar.Container>
+  );
+};
 
 Sidebar.Container = styled.aside`
-  position: fixed;
-  width: 20rem;
-  top: 7rem;
-  -webkit-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
-  -moz-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
-  box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+  ${({ open }) => `
+    position: fixed;
+    background: white;
+    width: 20rem;
+    top: 6.75rem;
+    -webkit-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+    -moz-box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+    box-shadow: 1px 30px 35px -4px rgba(219, 219, 219, 1);
+    z-index: 1000;
 
-  @media (max-width: 1024px) {
-    display: none;
-  }
+    @media (max-width: 1024px) {
+      display: ${open ? 'block' : 'none'}
+    }
+`}
 `;
 
 export default Sidebar;
