c0a9814171e002c7f8b789ba5f35448f9f7412dc
unknown <fegoworks@hotmail.com>
2020-08-01 23:51:00 +0100

    chore: refactor codebase

--- src/components/UI/molecules/GridDetail.jsx
+++ src/components/UI/molecules/GridDetail.jsx
@@ -1,47 +1,72 @@
 /** @format */
 
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Text from '<atoms>/Text';
 import Button from '<atoms>/Button';
 import styled from 'styled-components';
+import { CartContext } from '<state>/CartContext';
 
-const button = (available) => {
-  if (available === 'Sold Out') {
-    return null;
-  }
+const GridDetail = ({ book }) => {
+  const myBook = book;
+  const { available, title, author, year, genre, price } = book;
+  const { addProduct, cartItems, increase } = useContext(CartContext);
+
+  const isInCart = (product) => {
+    return !!cartItems.find((item) => item.id === product.id);
+  };
+
   return (
-    <Button
-      color="whiteSmoke"
-      borderRadius="0.35rem"
-      padding="0.25rem 0.55rem"
-      background="limegreen"
-      fontSize="xxxs"
-    >
-      + Add to cart
-    </Button>
+    <GridDetail.Container>
+      <h5 className={available === 'Available' ? 'available' : 'borrowed'}>
+        {available}
+      </h5>
+      <Text fontSize="xs" color="black" fontWeight="bold">
+        {title}
+      </Text>
+      <div>
+        <p className="grid-item">{author}</p>
+        <p className="grid-item">{year}</p>
+      </div>
+      <div>
+        <p className="grid-item">Genre: {genre}</p>
+        <p className="grid-item">Price: ${price}.00</p>
+      </div>
+      {button(available, myBook)}
+    </GridDetail.Container>
   );
-};
 
-const GridDetail = ({ available, title, author, year, genre, labels }) => (
-  <GridDetail.Container>
-    <h5 className={available === 'Available' ? 'available' : 'borrowed'}>
-      {available}
-    </h5>
-    <Text fontSize="xs" color="black" fontWeight="bold">
-      {title}
-    </Text>
-    <div>
-      <p className="grid-item">{author}</p>
-      <p className="grid-item">{year}</p>
-    </div>
-    <div>
-      <p className="grid-item">Genre: {genre}</p>
-      <p className="grid-item">Labels: {labels}</p>
-    </div>
-    {button(available)}
-  </GridDetail.Container>
-);
+  function button(available, book) {
+    if (available === 'Sold Out') {
+      return null;
+    }
+
+    return isInCart(book) ? (
+      <Button
+        className="btn_more"
+        color="whiteSmoke"
+        borderRadius="0.35rem"
+        padding="0.25rem 0.55rem"
+        background="purple"
+        fontSize="xxxs"
+        handleClick={() => increase(book)}
+      >
+        Add more
+      </Button>
+    ) : (
+      <Button
+        color="whiteSmoke"
+        borderRadius="0.35rem"
+        padding="0.25rem 0.55rem"
+        background="limegreen"
+        fontSize="xxxs"
+        handleClick={() => addProduct(book)}
+      >
+        + Add to cart
+      </Button>
+    );
+  }
+};
 
 GridDetail.Container = styled.div`
   padding: 0.75rem;
@@ -58,6 +83,16 @@ GridDetail.Container = styled.div`
 
   div {
     margin: 0.5rem 0;
+  }
+
+  .btn_more:hover {
+    background: none;
+    border: 1px solid purple;
+    color: purple;
+  }
+
+  .btn_more:active {
+    border: 2px solid purple;
   }
 `;
 
