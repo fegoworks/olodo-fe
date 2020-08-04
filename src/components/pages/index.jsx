0c24e8a03515e47a255abad75c4760dd2141a526
unknown <fegoworks@hotmail.com>
2020-08-04 23:03:12 +0100

    feat(template): add context providers

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -3,20 +3,30 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from '<core>/routing';
-import { HamburgerProvider } from '@/state/HamburgerContext';
+
+import { HamburgerProvider } from '<state>/HamburgerContext';
+import { BooksContextProvider } from '<state>/BooksContext';
+import { CartContextProvider } from '<state>/CartContext';
 
 // import GlobalContext from '<state>/GlobalContext';
 
 const BaseRoutes = () => (
-  // <GlobalContext>
-  <HamburgerProvider>
-    <Switch>
-      {routes.default.map(({ exact, path, component }, index) => (
-        <Route key={index} exact={exact} path={path} component={component} />
-      ))}
-    </Switch>
-  </HamburgerProvider>
-  // </GlobalContext>
+  <BooksContextProvider>
+    <CartContextProvider>
+      <HamburgerProvider>
+        <Switch>
+          {routes.default.map(({ exact, path, component }, index) => (
+            <Route
+              key={index}
+              exact={exact}
+              path={path}
+              component={component}
+            />
+          ))}
+        </Switch>
+      </HamburgerProvider>
+    </CartContextProvider>
+  </BooksContextProvider>
 );
 
 export default BaseRoutes;
